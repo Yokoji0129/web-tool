da27346d4d424ceb27701904ed206f7591ec90df
YuseiWakabayasi <fowakuma3@icloud.com>
2024-04-01 09:49:46 +0900

    ログイン機能を作成しました

--- web-tool/routes/web.php
+++ web-tool/routes/web.php
@@ -22,7 +22,9 @@ Route::get('/account/data', 'App\Http\Controllers\MainController@all_account_dat
 Route::get('/account/{id}/{password}', 'App\Http\Controllers\MainController@make_account');//テストアカウント作成
 Route::get('/search/{id}', 'App\Http\Controllers\MainController@search_id');//対象idが存在しなければtrueを存在すればfalseを返します
 Route::post('/post', 'App\Http\Controllers\MainController@add_account');
-Route::post('/account/login', 'App\Http\Controllers\MainController@login');//製作途中
+Route::get('/account/login/{id}/{password}', 'App\Http\Controllers\MainController@testlogin');//getでログインできるかのお試し
+Route::post('/login', 'App\Http\Controllers\MainController@login');//ログイン機能passwordが一致すればokを一致しなければnoを返す
+Route::get('/re', 'App\Http\Controllers\MainController@session');//requestを覗く
 // Route::get('/{any}', function () {
 //     return view('app');
 // })->where('any', '.*');
