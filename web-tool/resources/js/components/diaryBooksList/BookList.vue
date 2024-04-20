c8ee8fc4c252d885a9b0c25916bd8d0a8e79866a
Yokoji0129 <132248053+Yokoji0129@users.noreply.github.com>
2024-04-20 16:07:14 +0900

    お気に入り追加ボタン追加。 日記を削除する際警告出すように変更

--- web-tool/resources/js/components/diaryBooksList/BookList.vue
+++ web-tool/resources/js/components/diaryBooksList/BookList.vue
@@ -18,23 +18,30 @@ const toggleBookPopup = (book) => {
   selectedBook.value = book;
 };
 
-const deleteBook = (diaryId) => {
-  loading.value = true;
+//日記の削除メソッド
+const deleteBook = (diaryId, diaryName) => {
+  //日記削除警告
+  if (window.confirm(diaryName + "を本当に削除しますか？")) {
+    loading.value = true;
 
-  axios
-    .post("/delete/diary", {
-      id: diaryId,
-    })
-    .then((response) => {
-      props.displayBooks();
-      toggleBookPopup();
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .finally(() => {
-      loading.value = false;
-    });
+    axios
+      .post("/delete/diary", {
+        id: diaryId,
+      })
+      .then((response) => {
+        props.displayBooks();
+        toggleBookPopup();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        loading.value = false;
+      });
+  } else {
+    // いいえが選択された場合の処理
+    console.log("いいえが選択されました");
+  }
 };
 </script>
 
@@ -63,7 +70,13 @@ const deleteBook = (diaryId) => {
         </div>
         <div class="book-select">
           <p class="book-open">日記を開く</p>
-          <p class="book-delete" @click="deleteBook(selectedBook[0].diary_id)">
+          <p class="favorite">お気に入り追加</p>
+          <p
+            class="book-delete"
+            @click="
+              deleteBook(selectedBook[0].diary_id, selectedBook[0].diary_name)
+            "
+          >
             日記を削除
           </p>
         </div>
@@ -120,7 +133,7 @@ const deleteBook = (diaryId) => {
   background-color: #4caf50;
   border: none;
   color: white;
-  padding: 15px 32px;
+  padding: 10px 5%;
   text-align: center;
   text-decoration: none;
   display: inline-block;
@@ -134,11 +147,30 @@ const deleteBook = (diaryId) => {
 .book-open:hover {
   background-color: #36783a;
 }
+
+.favorite {
+  background-color: #4c8caf;
+  border: none;
+  color: white;
+  padding: 10px 5%;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 2px 20px;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+
+.favorite:hover {
+  background-color: #365d78;
+}
 .book-delete {
   background-color: #af4c4c;
   border: none;
   color: white;
-  padding: 15px 32px;
+  padding: 10px 5%;
   text-align: center;
   text-decoration: none;
   display: inline-block;
@@ -314,7 +346,7 @@ const deleteBook = (diaryId) => {
   }
 }
 
-@media screen and (max-width: 1000px) {
+@media screen and (max-width: 1024px) {
   .popup-content {
     width: 70%;
   }
@@ -332,10 +364,16 @@ const deleteBook = (diaryId) => {
 
 @media screen and (max-width: 450px) {
   .book-open {
-    padding: 5%;
+    margin: 5px 0;
+  }
+  .favorite {
+    margin: 5px 0;
   }
   .book-delete {
-    padding: 5%;
+    margin: 5px 0;
+  }
+  .book-select {
+    flex-direction: column;
   }
   .diaries {
     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
@@ -352,55 +390,55 @@ const deleteBook = (diaryId) => {
     height: 230px;
   }
   .book-paper h2 {
-    background-color: #ffffff;
-    padding: 0px;
+    font-size: 16px;
+    padding: 0;
   }
   .book-backnumber-1 {
     border-left: 10px solid rgb(107, 191, 93);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
 
   .book-backnumber-2 {
     border-left: 10px solid rgb(255, 222, 155);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
 
   .book-backnumber-3 {
     border-left: 10px solid rgb(161, 142, 130);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
 
   .book-backnumber-4 {
     border-left: 10px solid rgb(110, 204, 230);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
 
   .book-backnumber-5 {
     border-left: 10px solid rgb(207, 150, 163);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
   .book-backnumber-6 {
     border-left: 10px solid rgb(242, 195, 201);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
   .book-backnumber-7 {
     border-left: 10px solid rgb(182, 176, 162);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
 
   .book-backnumber-8 {
     border-left: 10px solid rgb(233, 239, 235);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
 
   .book-backnumber-9 {
     border-left: 10px solid rgb(245, 202, 225);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
 
   .book-backnumber-10 {
     border-left: 10px solid rgb(196, 235, 171);
-    padding: 20px 0 134px 0;
+    padding: 30px 0 149px 0;
   }
 }
 </style>
