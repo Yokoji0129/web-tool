1f359eeb7bfb6d4be2acb5b90a9bbd88f342fb64
Yokoji0129 <132248053+Yokoji0129@users.noreply.github.com>
2024-05-10 15:26:16 +0900

    デザイン調整

--- web-tool/resources/js/vue-views/Diaryview.vue
+++ web-tool/resources/js/vue-views/Diaryview.vue
@@ -1,12 +1,18 @@
 <script setup>
+import { RouterLink, useRouter } from "vue-router";
 import { ref } from "vue";
 import NavList from "../components/diary/NavList.vue";
 const menuOpen = ref(false);
+const router = useRouter();
 
 //ハンバーガーメニューの開閉
 const toggleMenu = () => {
   menuOpen.value = !menuOpen.value;
 };
+
+const backPage = () => {
+  router.push("/diaryBooksList")
+}
 </script>
 
 <template>
@@ -16,13 +22,25 @@ const toggleMenu = () => {
       <nav :class="{ open: menuOpen }" @click="closeMenu">
         <!--メニューアイコン-->
         <div class="hamburger-icon-menu" @click="toggleMenu()"></div>
-        <h1 class="diary-title">日記タイトル</h1>
-        <h2 class="table-contents">目次</h2>
+        <p class="back-page" @click="backPage">←日記一覧に戻る</p>
+        <h3 class="diary-title">日記タイトル</h3>
+        <h3 class="table-contents">目次</h3>
         <!--目次内容(menuOpenがtrueでスクロールできるようにする)-->
-        <div
-          class="table-contents-box"
-          :style="{ overflow: menuOpen ? 'scroll' : 'hidden' }"
-        >
+        <div class="table-contents-box" :style="{ overflow: menuOpen ? 'scroll' : 'hidden' }">
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
+          <p class="nav-link">0.test</p>
           <p class="nav-link">0.test</p>
           <p class="nav-link">0.test</p>
           <p class="nav-link">0.test</p>
@@ -42,29 +60,42 @@ const toggleMenu = () => {
           <p class="nav-link">0.test</p>
           <p class="nav-link">0.test</p>
         </div>
-        <!--ユーザー情報-->
-        <h2 class="username">ユーザー名: test</h2>
       </nav>
     </header>
   </div>
 </template>
 
 <style scoped>
+.back-page {
+  cursor: pointer;
+  position: absolute;
+  top: 3px;
+  left: 3px;
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px;
+  transition: 0.3s;
+}
+.back-page:hover {
+  background-color: rgba(74, 73, 73, 0.5);
+}
+
 .hamburger-icon,
 .hamburger-icon-menu {
   cursor: pointer;
   background-size: contain;
-  width: 60px;
-  height: 60px;
-  background-image: url(../../../public/icon/bars_hoso.png);
+  width: 40px;
+  height: 40px;
+  background-image: url(../../../public/icon/hamburger.png);
   align-self: flex-end;
-  margin: 10px 10px 0 0;
+  margin: 5px 10px 0 0;
   transition: 0.3s;
 }
 
 .hamburger-icon:hover,
 .hamburger-icon-menu:hover {
-  transform: scale(1.2);
+  transform: scale(1.1);
   transition: 0.3s ease;
 }
 
@@ -76,10 +107,10 @@ const toggleMenu = () => {
 nav {
   position: fixed;
   top: 82px;
-  left: -325px;
+  left: -340px;
   height: 100vh;
   width: 400px;
-  background-color: #ebebeb;
+  background-color: #ced4da;
   display: flex;
   flex-direction: column;
   transition: 0.3s;
@@ -96,32 +127,50 @@ nav p {
   text-decoration: none;
   color: black;
   transition: 0.3s;
+  font-family: "Georgia", serif;
 }
 
 .nav-link:hover {
-  background-color: #ebebeb;
+  background-color: #ced4da;
 }
 
 .diary-title {
   text-align: center;
+  font-family: "Georgia", serif;
   margin: 5px 0;
   padding: 5px 0;
-  background-color: #ebebeb;
+  background-color: #ced4da;
 }
 
 .table-contents {
   margin: 5px 10px;
+  font-family: "Georgia", serif;
 }
 
 .table-contents-box {
-  max-height: 30%; /**目次の高さが30%まで行ったらスクロールできるように**/
+  max-height: 70%;
+  /**目次の高さが30%まで行ったらスクロールできるように**/
 }
 
 .username {
-  background-color: #ebebeb;
-  margin: 5px 0;
+  background-color: #ced4da;
+  margin-top: 5px;
+  margin-bottom: 0;
   padding: 5px;
   text-align: center;
+}
+
+.logout {
+  cursor: pointer;
+  padding: 7px 0;
+  width: 100%;
+  background-color: #ced4da;
+  border: none;
+  transition: background-color 0.3s;
+}
+
+.logout:hover {
+  background-color: #a9aeb3;
 }
 
 .open {
@@ -132,25 +181,29 @@ nav p {
 
 /*タブレット(768px以下)*/
 @media screen and (max-width: 768px) {
+
   .hamburger-icon,
   .hamburger-icon-menu {
-    width: 50px;
-    height: 50px;
+    width: 40px;
+    height: 40px;
   }
+
   nav {
     top: 62px;
-    left: -280px;
+    left: -290px;
     width: 350px;
   }
 }
 
 /*スマホ(480px以下)*/
 @media only screen and (max-width: 450px) {
+
   .hamburger-icon,
   .hamburger-icon-menu {
     width: 40px;
     height: 40px;
   }
+
   nav {
     left: -290px;
   }
@@ -160,10 +213,10 @@ nav p {
   nav {
     top: 72px;
   }
+
   @media screen and (max-width: 768px) {
-    nav{
+    nav {
       top: 62px;
     }
   }
-}
-</style>
+}</style>
